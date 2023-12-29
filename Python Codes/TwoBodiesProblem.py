864f5913bd7f6f1dc0aecb9d575eb95504ce375d
stimch <59399058+Stimch@users.noreply.github.com>
2023-12-29 12:31:14 +0300

    Update TwoBodiesProblem.py

--- Python Codes/TwoBodiesProblem.py
+++ Python Codes/TwoBodiesProblem.py
@@ -2,52 +2,53 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-def model_2BP(state, t):
-    mu = 2.825E+014
+def model_2_body_problem(state, t):
+    gravitational_parameter = 3.248E+05
     x = state[0]
     y = state[1]
     z = state[2]
-    x_dot = state[3]
-    y_dot = state[4]
-    z_dot = state[5]
-    x_ddot = -mu * x / (x ** 2 + y ** 2 + z ** 2) ** (3 / 2)
-    y_ddot = -mu * y / (x ** 2 + y ** 2 + z ** 2) ** (3 / 2)
-    z_ddot = -mu * z / (x ** 2 + y ** 2 + z ** 2) ** (3 / 2)
-    dstate_dt = [x_dot, y_dot, z_dot, x_ddot, y_ddot, z_ddot]
+    x_velocity = state[3]
+    y_velocity = state[4]
+    z_velocity = state[5]
+    x_acceleration = -gravitational_parameter * x / (x ** 2 + y ** 2 + z ** 2) ** (3 / 2)
+    y_acceleration = -gravitational_parameter * y / (x ** 2 + y ** 2 + z ** 2) ** (3 / 2)
+    z_acceleration = -gravitational_parameter * z / (x ** 2 + y ** 2 + z ** 2) ** (3 / 2)
+    dstate_dt = [x_velocity, y_velocity, z_velocity, x_acceleration, y_acceleration, z_acceleration]
     return dstate_dt
-
+  
 # Начальные положения
-X_0 = -30000
-Y_0 = -55000
-Z_0 = 12000
-VX_0 = 1
-VY_0 = 0.8
-VZ_0 = 0.5
-state_0 = [X_0, Y_0, Z_0, VX_0, VY_0, VZ_0]
+satellite_initial_x = -30000
+satellite_initial_y = -55000
+satellite_initial_z = 12000
+satellite_initial_velocity_x = 1
+satellite_initial_velocity_y = 0.8
+satellite_initial_velocity_z = 0.5
+state_0 = [satellite_initial_x, satellite_initial_y, satellite_initial_z,
+           satellite_initial_velocity_x, satellite_initial_velocity_y, satellite_initial_velocity_z]
 
-t = np.linspace(0, 36 * 3600, 200)
+time = np.linspace(0, 36 * 3600, 200)
 
-sol = odeint(model_2BP, state_0, t)
-X_Sat = sol[:, 0]
-Y_Sat = sol[:, 1]
-Z_Sat = sol[:, 2]
+solution = odeint(model_2_body_problem, state_0, time)
+satellite_x = solution[:, 0]
+satellite_y = solution[:, 1]
+satellite_z = solution[:, 2]
 
-N = 50
-phi = np.linspace(0, 2 * np.pi, N)
-theta = np.linspace(0, np.pi, N)
+num_points = 50
+phi = np.linspace(0, 2 * np.pi, num_points)
+theta = np.linspace(0, np.pi, num_points)
 theta, phi = np.meshgrid(theta, phi)
 
-Juul_r = 6000  # Радиус Джула
-Juul_x = Juul_r * np.cos(phi) * np.sin(theta)
-Juul_y = Juul_r * np.sin(phi) * np.sin(theta)
-Juul_z = Juul_r * np.cos(theta)
+jool_radius = 6000  # Радиус планеты Jool
+jool_x = jool_radius * np.cos(phi) * np.sin(theta)
+jool_y = jool_radius * np.sin(phi) * np.sin(theta)
+jool_z = jool_radius * np.cos(theta)
 
 fig = plt.figure()
-axes = plt.axes(projection='3d')
-axes.plot_surface(Juul_x, Juul_y, Juul_z, color='green', alpha=0.7)
-axes.plot3D(X_Sat, Y_Sat, Z_Sat, 'purple')
+axes = fig.add_subplot(111, projection='3d')
+axes.plot_surface(jool_x, jool_y, jool_z, color='green', alpha=0.7)
+axes.plot3D(satellite_x, satellite_y, satellite_z, 'purple')
 axes.view_init(25, 145)
-plt.title('Задача двух тел')
+axes.set_title('Задача двух тел')
 axes.set_xlabel('X km')
 axes.set_ylabel('Y km')
 axes.set_zlabel('Z km')
